e6559db2510cde340101aaefb2a122be9a8e30e1
yarin1997 <155356573+yarin1997@users.noreply.github.com>
2024-09-25 09:28:48 +0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -5,8 +5,8 @@ const connectDB = async () => {
       console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
     const uri: string = process.env.ENV_MODE == "develop"
-      ? process.env.MONGODB_URI! || 'mongodb://localhost:27017/myapp-db'
-      : process.env.MONGO_ATLAS || '';
+      ? process.env.MONGODB_URI as string
+      : process.env.MONGO_ATLAS as string;
     if (!uri) {
       throw new Error('MongoDB connection string is not defined.');
     }
